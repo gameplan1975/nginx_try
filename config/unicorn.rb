22642c44e9696744e287cf7e883562a4c80e7318
gameplan1975 <gameplan1975@gmail.com>
2020-07-18 17:41:05 +0900

    through the path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,21 +1,21 @@
-#rails_root = File.expand_path('../../', __FILE__)
+rails_root = File.expand_path('../../', __FILE__)
 
-#worker_processes 2
-#working_directory rails_root
+worker_processes 2
+working_directory rails_root
 
-#listen "#{rails_root}/tmp/unicorn.sock"
-#pid "#{rails_root}/tmp/unicorn.pid"
+listen "#{rails_root}/tmp/unicorn.sock"
+pid "#{rails_root}/tmp/unicorn.pid"
 
-#stderr_path "#{rails_root}/log/unicorn_error.log"
-#stdout_path "#{rails_root}/log/unicorn.log"
+stderr_path "#{rails_root}/log/unicorn_error.log"
+stdout_path "#{rails_root}/log/unicorn.log"
 
-require 'fileutils'
-preload_app true
-timeout 5
-worker_processes 4
-listen '/tmp/nginx.socket'
+#require 'fileutils'
+#preload_app true
+#timeout 5
+#worker_processes 4
+#listen '/tmp/nginx.socket'
 #backlog: 1024
 
-before_fork do |server,worker|
-	FileUtils.touch('/tmp/app-initialized')
-end
+#before_fork do |server,worker|
+#	FileUtils.touch('/tmp/app-initialized')
+#end
